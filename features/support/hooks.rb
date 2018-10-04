16dd74099e48fc4e93cd98258b8d7d8fedd90815
testesequalidade <42354100+testesequalidade@users.noreply.github.com>
2018-10-04 15:41:25 -0300

    Elaboracao de pre-condicoes

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -9,3 +9,17 @@ After do |scenario|
     take_screenshot(scenario_name.downcase!, 'passed')
   end
 end
+
+# Acessa a url da aplicacao sem efetuar login
+Before('@acesso_mar_aberto') do
+  # pending
+end
+
+# Adiciona produto ao carrinho sem logar na aplicacao
+Before('@produto_carrinho_deslogado') do
+  # pending
+end
+
+Before('@produto_carrinho_logado') do
+  # pending
+end
